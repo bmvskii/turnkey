4ea17569a2d0c72c08183692afc3c632597ed660
Evgeniy Rogovoy <evgeniy.rogovoy.avizorsoft@gmail.com>
2018-09-06 13:38:33 +0300

    update v.1

--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-  //Если ошибок нет, отправить email
-    $emailTo = 'info@avezorsoftware.com'; //Сюда введите Ваш email
-    $body = "Name:" . $_POST["name"] . "\n\nEmail: " . $_POST["email"] . "\n\nPhone:" . $_POST["phone"] . "\n\nMessage:\n" . $_POST["message"];
-    $headers = 'From: Blockchain Avezorsoftware <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $_POST["email"];
-    mail($emailTo, $subject, $body, $headers);
-    $emailSent = true;
-?>
