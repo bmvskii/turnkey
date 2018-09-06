6757d51e3fadc55447b4e2861bc5026b80a2e9a5
Evgeniy Rogovoy <evgeniy.rogovoy.avizorsoft@gmail.com>
2018-09-06 15:02:11 +0300

    change php form

--- public/index.php
+++ public/index.php
@@ -1,16 +1,41 @@
 <?php
   //Если ошибок нет, отправить email
-    if (isset($_REQUEST['submit'])
-     && isset($_REQUEST['name'])
-     && isset($_REQUEST['email'])
-     && isset($_REQUEST['phone'])
-     && isset($_REQUEST['message'])) {
-      $emailTo = 'info@avezorsoftware.com'; //Сюда введите Ваш email
+  if(isset($_POST['submit'])) {
+    //Проверка Поля ИМЯ
+     if(trim($_POST['name']) == '') {
+      $hasError = true;
+     } else {
+     $name = trim($_POST['name']);
+     }
+    //Проверка правильности ввода EMAIL
+     if(trim($_POST['email']) == '')  {
+      $hasError = true;
+     } else {
+      $email = trim($_POST['email']);
+     }
+     if(trim($_POST['phone']) == '')  {
+      $hasError = true;
+     } else {
+      $phone = trim($_POST['phone']);
+     }
+    //Проверка наличия ТЕКСТА сообщения
+     if(trim($_POST['message']) == '') {
+      $hasError = true;
+     } else {
+      if(function_exists('stripslashes')) {
+        $message = stripslashes(trim($_POST['message']));
+      } else {
+        $message = trim($_POST['message']);
+      }
+     } {
+      if(!isset($hasError)) {
+        $emailTo = 'evg.rogovoy@gmail.com'; //Сюда введите Ваш email
       
-      $body = "Name:" . $_REQUEST["name"] . "\n\nEmail: " . $_REQUEST["email"] . "\n\nPhone:" . $_REQUEST["phone"] . "\n\nMessage:\n" . $_REQUEST["message"];
-      $headers = 'From: Blockchain Avezorsoftware <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $_REQUEST["email"];
-      $subject = 'Avezorsoftware Blockchain Contact Us';
+        $body = "Name:" . $name . "\n\nEmail: " . $email . "\n\nPhone:" . $phone . "\n\nMessage:\n" . $message;
+        $headers = 'From: Blockchain Avezorsoftware <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+        $subject = 'Avezorsoftware Blockchain Contact Us';
       
-      mail($emailTo, $subject, $body, $headers);  
+        mail($emailTo, $subject, $body, $headers);
+      }  
      }
 ?>
