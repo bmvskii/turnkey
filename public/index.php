624610ea1993d68c976aad1199c693f384341507
Evgeniy Rogovoy <evgeniy.rogovoy.avizorsoft@gmail.com>
2018-09-06 12:59:52 +0300

    fix php send mail form

--- public/index.php
+++ public/index.php
@@ -1,8 +1,8 @@
 <?php
   //Если ошибок нет, отправить email
     $emailTo = 'info@avezorsoftware.com'; //Сюда введите Ваш email
-    $body = "Name: $name \n\nEmail: $email \n\nPhone: $phone \n\nMessage:\n $message";
-    $headers = 'From: Blockchain Avezorsoftware <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email;
+    $body = "Name:" . $_POST["name"] . "\n\nEmail: " . $_POST["email"] . "\n\nPhone:" . $_POST["phone"] . "\n\nMessage:\n" . $_POST["message"];
+    $headers = 'From: Blockchain Avezorsoftware <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $_POST["email"];
     mail($emailTo, $subject, $body, $headers);
     $emailSent = true;
 ?>
