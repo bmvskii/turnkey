dd467e6d00c551975f65b09bc307374eab69a80e
Evgeniy Rogovoy <evgeniy.rogovoy.avizorsoft@gmail.com>
2018-09-17 18:12:11 +0300

    rewrite back-end validation in a soft regime (script checks only an email field)

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+
+if (trim($_POST['name']) != '') {
+    $name = trim($_POST['name']);
+}
+
+if (trim($_POST['email']) == '') {
+    $hasError = true;
+} else {
+    $email = trim($_POST['email']);
+}
+
+if (trim($_POST['phone']) != '') {
+    $phone = trim($_POST['phone']);
+}
+
+if (trim($_POST['message']) != '') {
+    if (function_exists('stripslashes')) {
+        $message = stripslashes(trim($_POST['message']));
+    } else {
+        $message = trim($_POST['message']);
+    }
+}
+if (!isset($hasError)) {
+    $emailTo = 'info@avezorsoftware.com';
+
+    $body = "Name:" . $name . "\n\nEmail: " . $email . "\n\nPhone:" . $phone . "\n\nMessage:\n" . $message;
+    $headers = 'From: Blockchain Avezorsoftware <' . $emailTo . '>' . "\r\n" . 'Reply-To: ' . $email;
+    $subject = 'Avezorsoftware Blockchain Contact Us';
+}
+?>
