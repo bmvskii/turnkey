b72cc5fa63dec8b18eacb5a6e8406990d9dbbe65
Evgeniy Rogovoy <evg.rogovoy@gmail.com>
2019-05-26 12:34:55 +0300

    Add validation

--- frontend/script/index.js
+++ frontend/script/index.js
@@ -25,7 +25,6 @@ document.addEventListener('DOMContentLoaded', () => {
         });
       }
     }
-
   }
 
   const scrollToAnchor = function () {
@@ -88,12 +87,46 @@ document.addEventListener('DOMContentLoaded', () => {
   initAccordeons();
   window.dispatchEvent(new Event('resize'));
 
-  const validateForm = (e) => {
-    e.preventDefault();
-    const { currentTarget: form } = e;
+  const options = {
+    // Specify validation rules
+    rules: {
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      first_name: "required",
+      last_name: "required",
+      business_email: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        email: true
+      },
+      phone_number: {
+        required: true,
+        minlength: 10,
+        digits: true,
+      },
+      company_name: "required",
+    },
+    // Specify validation error messages
+    messages: {
+      first_name: "Please enter your firstname",
+      last_name: "Please enter your lastname",
+      phone_number: {
+        required: "Please provide a phone-number",
+        minlength: "Your password must be at least 10 characters long",
+        digits: "Phone number should consist only from digits"
+      },
+      business_email: "Please enter a valid email address",
+      company_name: "Please enter a company name",
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
+    }
   };
 
-  document.querySelector('#form').addEventListener('submit', (e) => {
-
-  });
+  $("#form").validate(options);
+  $(".contact-us__form").validate(options);
 });
